bc97b016c6573b4dfa468dd48e268e36bfdcc13d
abhijitsahoo99 <abhijitsahooas.01@gmail.com>
2025-01-05 20:58:42 +0530

    fixes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,8 +19,8 @@ export default async function Home() {
           Welcome to Wager
         </h1>
         <p className="text-xl sm:text-2xl leading-8 font-roboto text-[#000000]">
-          Create and join betting pools with your friends for life's biggest
-          moments.
+          Create and join betting pools with your friends for life&apos;s
+          biggest moments.
         </p>
         <div className="flex justify-center gap-4">
           <Button
