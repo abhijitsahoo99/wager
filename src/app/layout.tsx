5e679b0dbf8b76bee91ae20831d84948fb2b7e0f
abhijitsahoo99 <abhijitsahooas.01@gmail.com>
2025-01-07 02:03:29 +0530

    opengraph and favicon update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,6 +19,29 @@ export const metadata: Metadata = {
   title: "Wager",
   description:
     " Create and join betting pools with your friends for life's biggest moments.",
+  openGraph: {
+    title: "Wager",
+    description:
+      "Create and join betting pools with your friends for life's biggest moments.",
+    images: [
+      {
+        url: "/assets/wager.png", // Make sure to add this image in your public folder
+        width: 1200,
+        height: 630,
+        alt: "Wager - Create and join betting pools with your friends for life's biggest moments.",
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+    siteName: "Wager",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Wager",
+    description:
+      "Create and join betting pools with your friends for life's biggest moments.",
+    images: ["/assets/wager.png"],
+  },
 };
 
 export default function RootLayout({
